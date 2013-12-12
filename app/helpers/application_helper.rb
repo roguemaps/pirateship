0690b3cb4b809aa386060e793706ad83e6eda162
Jeff Parr <jeffparr100@gmail.com>
2013-12-12 15:42:28 -0800

    add body class and id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ module ApplicationHelper
     content_for(:page_title) { ['Rogue Maps', page_title].join(' - ') }
   end
 
+  def body_classes
+    controller.controller_path.split('/')
+  end
+
+  def add_body_classes(classes)
+    content_for(:body_classes) { body_classes.append(classes).flatten }
+  end
+
+  def body_id
+    controller.controller_path.split('/').append(controller.action_name).join('-')
+  end
+
+
+
 end
