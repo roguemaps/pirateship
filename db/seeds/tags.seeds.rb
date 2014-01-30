8cb400ecea259593ee93d84baea0b4635f704a19
Jeff Parr <jeffparr100@gmail.com>
2014-01-29 17:25:39 -0800

    misc fixes

--- db/seeds/tags.seeds.rb
+++ db/seeds/tags.seeds.rb
@@ -4,20 +4,24 @@ module SafeSeed
     if attributes.is_a?(Array)
       attributes.collect { |attr| create_or_update_by_name!(attr) }
     else
-      find_or_initialize_by({:name => attributes[:name]}).update!(attributes)
+      r = find_or_initialize_by({:name => attributes[:name]})
+      r.update!(attributes)
+      r.touch
     end
   end
 end
 
 Tag.extend SafeSeed
 
+start_time = Time.now
 
 Tag.create_or_update_by_name!([{ :name => "Gluten Free", :filterable => true },
   { :name => "GMO Free", :filterable => true }, 
   { :name => "Organic", :filterable => true },
   { :name => "Local", :filterable => true },
   { :name => "Kid Friendly", :filterable => true },
-  { :name => "Soy Free", :filterable => true },
-  { :name => "Egg Free", :filterable => true },
+  { :name => "Eco Friendly", :filterable => true },
   { :name => "Farm to Table", :filterable => true },
   { :name => "Biodynamic", :filterable => true }])
+
+Tag.where('updated_at < ?', start_time).delete_all
