f6d6a3e807cf3e0ee0a58ffb373f1b5fe9d10b00
Jeff Parr <jeffparr100@gmail.com>
2014-04-04 12:41:02 -0700

    set url for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Pirateship::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url => 'roguemaps.s3.amazonaws.com',
+    :url => ':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
     :s3_host_name => 's3.amazonaws.com'
   }
